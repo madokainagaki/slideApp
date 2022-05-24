675f92c39508e50e8273f88fdd07fd8c01f9899c
madokainagaki <jo6gencho9@gmail.com>
2022-05-24 23:38:56 +0900

    パーミッションまだ

--- app/src/main/java/jp/techacademy/madoka/inagaki/autoslideshowapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/madoka/inagaki/autoslideshowapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package jp.techacademy.madoka.inagaki.autoslideshowapp
 
 import android.content.ContentUris
+import android.database.Cursor
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
@@ -9,52 +10,20 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private var cursor : Cursor? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val resolver = contentResolver
-        val cursor = resolver.query(
+        cursor = resolver.query(
             MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
             null,
             null,
             null,
             null
         )
-
-        //起動時にひとまず表示。
-        cursor!!.moveToFirst()
-
-        fun view(){
-            val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
-            val id = cursor.getLong(fieldIndex)
-            val imageUri =
-                ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
-            imageView.setImageURI(imageUri)
-        }
-        view()
-
-        //次へ
-        fun nextView() {
-            if (cursor!!.isLast) {
-                cursor!!.moveToFirst()
-            } else {
-                cursor!!.moveToNext()
-            }
-            view()
-        }
-
-
-        //前へ
-        fun preView() {
-            if (cursor!!.isFirst) {
-                cursor.moveToLast()
-            } else {
-                cursor!!.moveToPrevious()
-            }
-            view()
-        }
 
         //ボタンのリスナー
         btnNext.setOnClickListener {
@@ -64,6 +33,42 @@ class MainActivity : AppCompatActivity() {
             preView()
         }
 
+        //起動時にひとまず表示。
+        cursor!!.moveToFirst()
+        view()
     }
+
+    fun view(){
+        val fieldIndex = cursor!!.getColumnIndex(MediaStore.Images.Media._ID)
+        val id = cursor!!.getLong(fieldIndex)
+        val imageUri =
+            ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+        imageView.setImageURI(imageUri)
+    }
+
+
+    //次へ
+    fun nextView() {
+        if (cursor!!.isLast) {
+            cursor!!.moveToFirst()
+        } else {
+            cursor!!.moveToNext()
+        }
+        view()
+    }
+
+
+    //前へ
+    fun preView() {
+        if (cursor!!.isFirst) {
+            cursor!!.moveToLast()
+        } else {
+            cursor!!.moveToPrevious()
+        }
+        view()
+    }
+
+
+
 }
 
